bf0d99604d91ec6f7b52ed784b88923a9edf4da7
benshabbat <benshabbat27@gmail.com>
2023-01-26 19:50:54 +0200

    checking connection

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ import mongoose from "mongoose";
 const app = express();
 dotenv.config();
 
-const connect =async()=>{
+const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
     console.log("connected to mongoDB!");
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!");
+});
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected!");
+});
 
 app.listen(8800, () => {
-  connect()
+  connect();
   console.log("connected to backend!");
 });
