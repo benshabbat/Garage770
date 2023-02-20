e081e8649038b32e1925a704f6002997e47f4bf7
benshabbat <benshabbat27@gmail.com>
2023-02-20 10:13:06 +0200

    create folder config for db

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import carsRoute from "./routes/cars.js";
@@ -8,43 +7,9 @@ import servicesRoute from "./routes/services.js";
 import messagesRoute from "./routes/messages.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import connectDB from "./config/db.js"
 const app = express();
 dotenv.config();
-
-const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO);
-    console.log("connected to mongoDB!");
-  } catch (error) {
-    throw error;
-  }
-};
-
-mongoose.connection.on("disconnected", () => {
-  console.log("MongoDB disconnected!");
-});
-mongoose.connection.on("connected", () => {
-  console.log("MongoDB connected!");
-});
-
-// app.use((req, res, next) => {
-//   // res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
-// app.use((req,res,next) =>{
-//   res.header("Access-Control-Allow-Origin","*");
-//   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   if(req==="OPTIONS"){
-//       res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-//       return res.status(200).json({});
-//   }
-//   next();
-// });
 
 //middlewares
 app.use(cookieParser());
@@ -60,6 +25,6 @@ app.use("/api/messages", messagesRoute);
 
 
 app.listen(8800, () => {
-  connect();
+  connectDB();
   console.log("connected to backend!");
 });
