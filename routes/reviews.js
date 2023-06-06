9521333d1619c7921aa27d092c07d8646e8819c8
benshabbat <benshabbat27@gmail.com>
2023-06-06 20:34:58 +0300

    routes reviews done

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,21 +1,9 @@
 import express from "express";
-import {
-    updateReview,
-    deleteReview,
-    getReview,
-    getReviews,
-    createReview,
-  } from "../controllers/review.js";
-  import { verifyAdmin, verifyToken,verifyUser } from "../utils/verifyToken.js";
-  const router = express.Router();
+import { getReviews, createReview } from "../controllers/review.js";
+const router = express.Router();
 
 //GET ALL
-router.get("/",verifyAdmin, getReviews);
-//CREATE
-router.post("/:userId",verifyAdmin,createReview);
-//UPDATE
-router.put("/:id",verifyAdmin, updateReview);
-//DELETE
-router.delete("/:id/:userId",verifyAdmin, deleteReview);
-//GET
-router.get("/:id",verifyUser, getReview);
+router.get("/", getReviews);
+
+//POST
+router.post("/", createReview);
